b9f9ba056f8cce8e102d483303e4dadb4cebde3e
godweiyang <792321264@qq.com>
2026-07-04 18:27:35 +0800

    v1.2.0 版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.videodownloader"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.1.3"
+        versionCode = 6
+        versionName = "1.2.0"
     }
 
     buildTypes {
